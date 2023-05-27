4128847c9f9412c9e5231a5e5b9ae978ba8abe72
Washington Ribeiro <washinuchiha@gmail.com>
2023-05-27 10:41:02 -0300

    feat: working with conditional rendering

--- src/App.jsx
+++ src/App.jsx
@@ -5,13 +5,17 @@ import MeuContador from "./components/MeuContador";
 function App() {
   return (
     <div>
+      {/* Aula 02 - Olá mundo em React */}
       <h1>Olá Mundo, React!</h1>
-      <MeuComponente/>
 
+      {/* Aula 03 - Componentes e props */}
+      <MeuComponente/>
       <MeuBotao conteudo='me clique'/>
       <MeuBotao conteudo='depois aqui'/>
       <MeuBotao conteudo='e por fim aqui'/>
 
+      {/* Aula 04 - Estados e eventos */}
+      {/* Aula 05 - Renderização condicional */}
       <MeuContador></MeuContador>
     </div>
   );
--- src/components/MeuContador.jsx
+++ src/components/MeuContador.jsx
@@ -1,18 +1,31 @@
 import { useState } from "react";
 
 export default function MeuContador() {
-  
-    const [cont, setCont] = useState(0)
+  const [cont, setCont] = useState(0);
 
-    function aumentar() {
-        setCont(cont + 1);
-    }
+  function aumentar() {
+    setCont(cont + 1);
+  }
+
+  function diminuir() {
+    setCont(cont - 1);
+  }
+
+  if (cont > 5) {
+    return (
+      <div>
+        <h1>Valor muito alto!</h1>
+        <button onClick={diminuir}> Diminuir </button>
+        <button onClick={aumentar}> Aumentar </button>
+      </div>
+    )
+  }
 
   return (
     <div>
-        <h1>Meu contador</h1>
-        <h3>{cont}</h3>
-        <button onClick={aumentar}>Aumentar</button>
+      <h1>Meu contador: {cont}</h1>
+      <button onClick={diminuir}> Diminuir </button>
+      <button onClick={aumentar}> Aumentar </button>
     </div>
   );
 }
