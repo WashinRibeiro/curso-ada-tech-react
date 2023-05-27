556b6c0e640afd2e0316476e90fc60719f49fcc6
Washington Ribeiro <washinuchiha@gmail.com>
2023-05-27 02:51:51 -0300

    feat: separating into components and understanding props

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import MeuBotao from "./components/MeuBotao";
+import MeuComponente from "./components/MeuComponente";
 
+function App() {
   return (
-    <h1>Olá Mundo, React!</h1>
-  )
+    <div>
+      <h1>Olá Mundo, React!</h1>
+      <MeuComponente/>
+
+      <MeuBotao conteudo='me clique'/>
+      <MeuBotao conteudo='depois aqui'/>
+      <MeuBotao conteudo='e por fim aqui'/>
+    </div>
+  );
 }
 
-export default App
+export default App;
---
+++ src/components/MeuComponente.jsx
@@ -0,0 +1,10 @@
+export default function MeuComponente() {
+  return (
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Natus repellendus
+      nisi, eos, saepe doloribus voluptatibus recusandae reiciendis a dolores
+      veritatis sint beatae mollitia voluptatem veniam nesciunt, blanditiis
+      tenetur iusto dolorem?
+    </p>
+  );
+}
